d98f0d971a1588c720a6336833d88ae8725de8a2
albert.fix <albert.fix@ssi-schaefer.com>
2017-02-23 14:59:32 +0100

    Test xlatGt

--- src/fix/mp3/demo/unicode/TranslateGt.java
+++ src/fix/mp3/demo/unicode/TranslateGt.java
@@ -1,15 +1,24 @@
 package fix.mp3.demo.unicode;
 
 import com.gtranslate.Translator;
+import com.gtranslate.text.Text;
+import com.gtranslate.text.TextTranslate;
 
 public class TranslateGt {
 
 	public static void main(String[] args) {
-		String text = "Sergej Minaev - Gimn Diskoteki";
+		String input = "Sergej Minaev - Gimn Diskoteki";
 		Translator translator = Translator.getInstance();
-		// System.out.println(translator.detect(text));
-		System.out.println(translator.translate(text, "de", "ru"));
 
+		// System.out.println(translator.detect(text));
+
+		System.out.println(translator.translate(input, "de", "ru"));
+
+		String output = "";
+		Text text = new Text(input, "ru");
+		TextTranslate textTranslate = new TextTranslate(text, output);
+		translator.translate(textTranslate);
+		System.out.println(textTranslate.toString());
 	}
 
 }
